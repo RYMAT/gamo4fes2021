e7bb02c955256f0892d2c05b552d1bea65efded2
ryo_matsuuchi <matsuuchi.ryo@suzukishouten.co.jp>
2021-01-30 21:32:28 +0900

    setEnv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gamoyon';
+
+  ngOnInit(): void {
+    console.log(
+      environment.APP_FIREBASE_KEY,
+      environment.APP_FIREBASE_DOMAIN,
+      environment.APP_FIREBASE_PROJECT_ID,
+      environment.APP_FIREBASE_STORAGE_BUCKET,
+      environment.APP_FIREBASE_SENDER_ID,
+    );
+  }
 }
